20702edf4c45205aae75d20702b3469364a561b9
Carlos Coves Prieto <escorponox@gmail.com>
2022-02-26 17:18:38 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const apolloHooks = [
+  "useQuery",
+  "useLazyQuery",
+  "useMutation",
+  "useSubscription",
+];
+
+module.exports = {
+  rules: {
+    "no-smart-import-rename": {
+      create: function (context) {
+        return {
+          ImportSpecifier: function (node) {
+            if (
+              apolloHooks.includes(node.imported.name) &&
+              node.local.name !== node.imported.name
+            ) {
+              context.report({
+                node: node,
+                message: "Forbidden import rename, don't be too smart",
+              });
+            }
+          },
+        };
+      },
+    },
+    "no-untyped-apollo-hooks": {
+      create: function (context) {
+        return {
+          Identifier: function (node) {
+            if (
+              node.name === "useQuery" &&
+              node.parent.type === "CallExpression" &&
+              (node.parent.typeParameters === undefined ||
+                node.parent.typeParameters.params.length < 2)
+            ) {
+              context.report({
+                node: node,
+                message: "Type your Apollo hooks, you won't regret it",
+              });
+            }
+          },
+        };
+      },
+    },
+  },
+};
